25e047d34064f0404e5895cdf90ee75a0fcca2ce
KitKatFarrelly <pfarrellyncsu@gmail.com>
2025-12-29 11:15:56 -0500

    uses the sum method

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 use std::ops::Add;
+use std::iter::Sum;
 
 trait generic_sum<A>
 {
     fn gen_sum(self) -> A;
 }
 
-impl<T: IntoIterator<Item=A>, A: Add<Output = A> + Default> generic_sum<A> for T
+impl<T: IntoIterator<Item=A>, A: Sum> generic_sum<A> for T
 {
     fn gen_sum(self) -> A
     {
-        let mut ret_val: A = Default::default();
-
         let mut iterator = self.into_iter();
 
+        /*
         while let Some(current) = iterator.next()
         {
             ret_val = ret_val + current;
         }
-        ret_val
+        */
+        iterator.sum::<A>()
     }
 }
 
